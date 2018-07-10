4b1621da69b1f4d11cff77562b3439ea2cf90c88
Peter Nosko <peter.nosko@gmail.com>
2018-07-10 23:46:05 +0200

    Change the name DecentExtensionAdapter -> AdapterDecentExtension, because GB API is fuckin retarded

---
+++ src/main/scala/com/generalbytes/batm/server/extensions/extra/decent/AdapterDecentExtension.scala
@@ -0,0 +1,6 @@
+package com.generalbytes.batm.server.extensions.extra.decent
+
+import common.ExtensionAdapter
+
+// WARN: DON'T change the name. Name must end with the word "Extension", because GeneralBytes' API is retarded like that.
+class AdapterDecentExtension extends ExtensionAdapter(new DecentExtension())
--- src/main/scala/com/generalbytes/batm/server/extensions/extra/decent/DecentExtensionAdapter.scala
+++
@@ -1,5 +0,0 @@
-package com.generalbytes.batm.server.extensions.extra.decent
-
-import common.ExtensionAdapter
-
-class DecentExtensionAdapter extends ExtensionAdapter(new DecentExtension())
